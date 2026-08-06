99d8881214d499bf234cae5b7ffae0077c75034c
Sabhyata Bharti Jha <sabhyatabharti808@gmail.com>
2026-08-06 20:00:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.67%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        if(n<=0) return false;
+
+        int x=0;
+
+        while(pow(2,x)<=n){
+            if(pow(2,x)==n)
+            return true;
+            x++;
+        }
+        return false;
+        
+        
+    }
+};
